646611586bd77e11280f9d2a01366a659138c87d
Vladimir Skipin <skipin_v@mail.ru>
2017-11-20 11:04:28 +0500

    Create form-example.component.ts

---
+++ form-example.component.ts
@@ -0,0 +1,17 @@
+ ngOnInit() {
+    this.registrationForm = this.formBuilder.group({
+      lastName: ['', Validators.required ],
+      firstName: ['', Validators.required ],
+      patronymic: [''],
+      email: ['', [
+        Validators.required,
+        // Validators.pattern(emailValidator.pattern),
+        ValidationService.emailValidator,
+      ]],
+      password: [null, Validators.required ],
+      passwordConfirm: [null ],
+      usefulInformation: [true],
+      isAccept: [false],
+      recommend: [null],
+    }, { validator: ValidationService.matchPass});
+  }
